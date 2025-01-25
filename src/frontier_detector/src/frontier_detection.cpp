d4f7d54fe32b8394c105a64d7ddd16efe25bb072
go69rex <go69rex@mytum.de>
2025-01-25 23:00:15 +0100

    removing bug and extra checking files from frontier detector used to view the frontier

--- src/frontier_detector/src/frontier_detection.cpp
+++ src/frontier_detector/src/frontier_detection.cpp
@@ -1,22 +1,19 @@
-#include <ros/ros.h>
-#include <octomap/octomap.h>
-#include <octomap_msgs/Octomap.h> //// http://docs.ros.org/en/noetic/api/octomap_msgs/html/msg/Octomap.html
-#include <octomap_msgs/conversions.h> //http://docs.ros.org/en/jade/api/octomap_msgs/html/conversions_8h.html
-#include <visualization_msgs/MarkerArray.h>
-#include <visualization_msgs/Marker.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/Point.h>
-#include <tf/transform_broadcaster.h>
+#include <ros/ros.h>						// 
+#include <octomap/octomap.h>				// 
+#include <octomap_msgs/Octomap.h> 			// http://docs.ros.org/en/noetic/api/octomap_msgs/html/msg/Octomap.html
+#include <octomap_msgs/conversions.h> 		// http://docs.ros.org/en/jade/api/octomap_msgs/html/conversions_8h.html
+#include <visualization_msgs/MarkerArray.h>	// https://docs.ros.org/en/noetic/api/visualization_msgs/html/msg/MarkerArray.html
+#include <visualization_msgs/Marker.h>		// https://docs.ros.org/en/noetic/api/visualization_msgs/html/msg/Marker.html
+#include <geometry_msgs/PoseStamped.h>		// A Pose with reference coordinate frame and timestamp Header header Pose pose
+#include <geometry_msgs/Pose.h>				// A representation of pose in free space, composed of position and orientation. Point position Quaternion orientation
+#include <geometry_msgs/Point.h> 			// This contains the position of a point in free space float64 x float64 y float64 z
+#include <tf/transform_broadcaster.h>		//
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <custom_msgs/FrontierGoalMsg.h>
-#include <std_msgs/String.h>
-#include <std_srvs/Empty.h>
-
+#include <custom_msgs/FrontierGoalMsg.h>	//
+#include <std_msgs/String.h>				//
+#include <std_srvs/Empty.h>					//
 
 #define TOL 1
-
-
 
 class FrontierDetector{
 private:
@@ -25,59 +22,56 @@ private:
 	};
 	
 	struct Frontier{
-		Point3D point;
-		int count = 0;
+		Point3D coordinates;
+		int neighborcount = 0;
 		double score = 0;
-		bool raytraceable = false;
+		bool isReachable = false;
 	};
 
-
     ros::NodeHandle nh;
-    ros::Subscriber octomap_sub, pos_sub, goal_sub, state_sub;
-    ros::Publisher frontier_pub, goal_pub, frontier_goal_pub;
+    ros::Subscriber octomap_subscriber, Curr_Pose_Subscriber, goal_subscriber, state_subscriber;
+    ros::Publisher frontier_publisher, goal_publisher, frontier_goal_publisher;
     ros::Timer timer;
 
     double drone_yaw;
-    Point3D drone_pos, goal_pos_sub, cave_entry;
-	geometry_msgs::PoseStamped goal_msg;
-	custom_msgs::FrontierGoalMsg frontier_goal_msg;	
+    Point3D curr_drone_position, goal_Curr_Pose_Subscriber, cave_entry_point;
+	geometry_msgs::PoseStamped goal_message;
+	custom_msgs::FrontierGoalMsg frontier_goal_message;	
 	float octomap_res;
-	std::string stm_state;
+	std::string statemachine_state;
 	
 	
-	// score function gains
-	double k_dist = 1.0;
-	double k_count = 0.1;
+	// score function gains to rank the frontiers (Parameter Tunning needed)
+	double k_distance = 1.0;
+	double k_neighborcount = 0.1;
 	double k_yaw = 30.0;
 
 
 public:
     FrontierDetector(){
-        octomap_sub = nh.subscribe("/octomap_binary", 1, &FrontierDetector::parseOctomap, this);
+        octomap_subscriber = nh.subscribe("/octomap_binary", 1, &FrontierDetector::parseOctomap, this);
 		timer = nh.createTimer(ros::Duration(1.0), &FrontierDetector::publish_goal,this);
-        frontier_pub = nh.advertise<visualization_msgs::MarkerArray>("/frontiers", 1000);
-        pos_sub = nh.subscribe("/pose_est",1,&FrontierDetector::onPose,this);
-        goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/goal", 1000);
-        frontier_goal_pub = nh.advertise<custom_msgs::FrontierGoalMsg>("/frontiergoal", 1000);
-        // state_sub = nh.subscribe("/Current_State_stm", 1, &FrontierDetector::onStateStm, this);
+        frontier_publisher = nh.advertise<visualization_msgs::MarkerArray>("/frontiers", 1000);
+        Curr_Pose_Subscriber = nh.subscribe("/pose_est",1,&FrontierDetector::Current_position,this);
+        goal_publisher = nh.advertise<geometry_msgs::PoseStamped>("/goal", 1000);
+        frontier_goal_publisher = nh.advertise<custom_msgs::FrontierGoalMsg>("/frontier_goal", 1000);
+        // state_subscriber = nh.subscribe("/Current_State_stm", 1, &FrontierDetector::onStateStm, this);
         
         // cave entry coordinates
-		cave_entry.x = -321.0;
-		cave_entry.y = 10.0;
-		cave_entry.z = 15.0;
-		
-		goal_msg.header.frame_id = "world";
+		cave_entry_point.x = -321.0;
+		cave_entry_point.y = 10.0;
+		cave_entry_point.z = 15.0;
     }
     
     // void onStateStm(const std_msgs::String& cur_state){
-	// 	stm_state = cur_state.data;
+	// 	statemachine_state = cur_state.data;
 	// }
 		
     
-    void onPose(const geometry_msgs::PoseStamped& msg){
-    	drone_pos.x = msg.pose.position.x;
-    	drone_pos.y = msg.pose.position.y;
-    	drone_pos.z = msg.pose.position.z;
+    void Current_position(const geometry_msgs::PoseStamped& msg){
+    	curr_drone_position.x = msg.pose.position.x;
+    	curr_drone_position.y = msg.pose.position.y;
+    	curr_drone_position.z = msg.pose.position.z;
     	
     	tf2::Quaternion quat;
     	tf2::fromMsg(msg.pose.orientation, quat);
@@ -88,11 +82,11 @@ public:
 		mat.getRPY(roll, pitch, drone_yaw);
 
 		// Check if drone has reached the cave entry point
-        if (abs(drone_pos.x - cave_entry.x) < 1.0 && abs(drone_pos.y - cave_entry.y) < 1.0 && abs(drone_pos.z - cave_entry.z) < 1.0 && abs(drone_yaw - 3.14) < 0.1) {
-            if (stm_state != "Explore Cave") {
+        if (abs(curr_drone_position.x - cave_entry_point.x) < 1.0 && abs(curr_drone_position.y - cave_entry_point.y) < 1.0 && abs(curr_drone_position.z - cave_entry_point.z) < 1.0 && abs(drone_yaw - 3.14) < 0.1) {
+            if (statemachine_state != "Explore Cave") {
                 ROS_INFO("Reached cave entry. Starting cave exploration.");
                 ros::param::set("/Current_State", "Explore Cave");
-				stm_state="Explore Cave";
+				statemachine_state="Explore Cave";
                 resetOctomap();
             }
         }
@@ -122,10 +116,10 @@ public:
 	// Perform mean shift clustering - implemented with help from chatgpt
 	std::vector<Point3D> meanShiftClustering(std::vector<Frontier> points, double bandwidth, double convergenceThreshold) {
 		std::vector<Point3D> shiftedPoints;
-		ROS_INFO("starting meanShiftClustering");
+		ROS_INFO("MeanShiftClustering algorithm starting");
 		
 		for(Frontier frontier:points){
-			shiftedPoints.push_back(frontier.point);
+			shiftedPoints.push_back(frontier.coordinates);
 		}
 
 		bool converged = false;
@@ -140,12 +134,12 @@ public:
 		        double totalWeight = 0.0;
 
 		        for (const Frontier& p : points) {
-		            double distance = euclideanDistance(originalPoint, p.point);
+		            double distance = euclideanDistance(originalPoint, p.coordinates);
 		            double weight = gaussianKernel(distance, bandwidth);
 
-		            shiftVector.x += p.point.x * weight;
-		            shiftVector.y += p.point.y * weight;
-		            shiftVector.z += p.point.z * weight;
+		            shiftVector.x += p.coordinates.x * weight;
+		            shiftVector.y += p.coordinates.y * weight;
+		            shiftVector.z += p.coordinates.z * weight;
 
 		            totalWeight += weight;
 		        }
@@ -171,38 +165,39 @@ public:
 	}
 	
 	double get_score(Frontier frontier){		
-		double yaw_score = abs(atan2(frontier.point.y-drone_pos.y, frontier.point.x-drone_pos.x)-drone_yaw);
+		double yaw_score = abs(atan2(frontier.coordinates.y-curr_drone_position.y, frontier.coordinates.x-curr_drone_position.x)-drone_yaw);
     	if(yaw_score > 3.14){
     		yaw_score = 6.28-yaw_score;
     	}
 		ROS_INFO("calculating score");
-    	return -k_dist*euclideanDistance(frontier.point, drone_pos) + k_count*(double)frontier.count - k_yaw*yaw_score;
+    	return -k_distance*euclideanDistance(frontier.coordinates, curr_drone_position) + k_neighborcount*(double)frontier.neighborcount - k_yaw*yaw_score;
 	}
 	
 	void publish_goal(const ros::TimerEvent& event){
 		Point3D goal_point;
-		goal_point.x = goal_msg.pose.position.x;
-		goal_point.y = goal_msg.pose.position.y;
-		goal_point.z = goal_msg.pose.position.z;
-		if(stm_state == "Explore Cave"){
-	    	goal_pub.publish(goal_msg);
-	    	frontier_goal_pub.publish(frontier_goal_msg);
+		goal_point.x = goal_message.pose.position.x;
+		goal_point.y = goal_message.pose.position.y;
+		goal_point.z = goal_message.pose.position.z;
+		if(statemachine_state == "Explore Cave"){
+	    	goal_publisher.publish(goal_message);
+	    	frontier_goal_publisher.publish(frontier_goal_message);
 			ROS_INFO(" publishing goal ");
 	    }
 	}
 	
-	void set_goal_msg(Frontier best_frontier){
-		goal_msg.header.frame_id = "world";
-	    goal_msg.pose.position.x = best_frontier.point.x;
-	    goal_msg.pose.position.y = best_frontier.point.y;
-	    goal_msg.pose.position.z = best_frontier.point.z;
-	    double goal_yaw = atan2(best_frontier.point.y-drone_pos.y,best_frontier.point.x-drone_pos.x);
+	void set_goal_message(Frontier best_frontier){
+		goal_message.header.frame_id = "world";
+	    goal_message.pose.position.x = best_frontier.coordinates.x;
+	    goal_message.pose.position.y = best_frontier.coordinates.y;
+	    goal_message.pose.position.z = best_frontier.coordinates.z;
+	    double goal_yaw = atan2(best_frontier.coordinates.y-curr_drone_position.y,best_frontier.coordinates.x-curr_drone_position.x);
 	    tf2::Quaternion q;
 		q.setRPY( 0, 0, goal_yaw);
-		goal_msg.pose.orientation.x = q.getX();
-	    goal_msg.pose.orientation.y = q.getY();
-	    goal_msg.pose.orientation.z = q.getZ();
-	    goal_msg.pose.orientation.w = q.getW();
+		goal_message.pose.orientation.x = q.getX();
+	    goal_message.pose.orientation.y = q.getY();
+	    goal_message.pose.orientation.z = q.getZ();
+	    goal_message.pose.orientation.w = q.getW();
+
 		ROS_INFO("setting goal msg  ");
 		
 	}
@@ -210,25 +205,21 @@ public:
 	void select_frontier(std::vector<Frontier> points_sorted){
 		if(points_sorted.size()>0){
 		    Frontier best_frontier = points_sorted[0];
-		    best_frontier.score = get_score(best_frontier);
-		    
+		    best_frontier.score = get_score(best_frontier);		    
 		    for(Frontier frontier:points_sorted){
-		    	frontier.score = get_score(frontier);
-		    	//if(euclideanDistance(f3.point, drone_pos) < euclideanDistance(closest_frontier.point, drone_pos)){
-		    	//if(f3.count > closest_frontier.count){
+		    	frontier.score = get_score(frontier);		    	
 		    	if(frontier.score > best_frontier.score){
-		    		best_frontier = frontier;
-		    		
+		    		best_frontier = frontier;		    		
 		    	}
 		    }
-		    frontier_goal_msg.point.x = best_frontier.point.x;
-	    	frontier_goal_msg.point.y = best_frontier.point.y;
-	    	frontier_goal_msg.point.z = best_frontier.point.z;
-	    	frontier_goal_msg.raytraceable = best_frontier.raytraceable;
+		    frontier_goal_message.point.x = best_frontier.coordinates.x;
+	    	frontier_goal_message.point.y = best_frontier.coordinates.y;
+	    	frontier_goal_message.point.z = best_frontier.coordinates.z;
+	    	frontier_goal_message.isReachable = best_frontier.isReachable;
 			ROS_INFO("  selecting frontiers");
 	    	
-			if(best_frontier.raytraceable && euclideanDistance(drone_pos, best_frontier.point) < 40){
-		    	set_goal_msg(best_frontier);
+			if(best_frontier.isReachable && euclideanDistance(curr_drone_position, best_frontier.coordinates) < 40){
+		    	set_goal_message(best_frontier);
 		    	
 		    }
         }
@@ -244,35 +235,35 @@ public:
 		frontierMarkers.markers.push_back(clear_marker);
             
         for(int i=0; i<points_sorted.size();i++){
-        	visualization_msgs::Marker marker;
-				marker.header.frame_id = "world";
-				marker.header.stamp = ros::Time::now();
-				marker.ns = "frontier";
-				marker.id = i+1;
-				marker.type = visualization_msgs::Marker::CUBE;
+        	visualization_msgs::Marker new_frontier_marker;
+				new_frontier_marker.header.frame_id = "world";
+				new_frontier_marker.header.stamp = ros::Time::now();
+				new_frontier_marker.ns = "frontier";
+				new_frontier_marker.id = i+1;
+				new_frontier_marker.type = visualization_msgs::Marker::CUBE;
 				
-				marker.pose.position.x = points_sorted[i].point.x;
-				marker.pose.position.y = points_sorted[i].point.y;
-				marker.pose.position.z = points_sorted[i].point.z;
-				marker.pose.orientation.x = 0.0;
-				marker.pose.orientation.y = 0.0;
-				marker.pose.orientation.z = 0.0;
-				marker.pose.orientation.w = 1.0;
+				new_frontier_marker.pose.position.x = points_sorted[i].coordinates.x;
+				new_frontier_marker.pose.position.y = points_sorted[i].coordinates.y;
+				new_frontier_marker.pose.position.z = points_sorted[i].coordinates.z;
+				new_frontier_marker.pose.orientation.x = 0.0;
+				new_frontier_marker.pose.orientation.y = 0.0;
+				new_frontier_marker.pose.orientation.z = 0.0;
+				new_frontier_marker.pose.orientation.w = 1.0;
 				
-				marker.scale.x = octomap_res;
-				marker.scale.y = octomap_res;
-				marker.scale.z = octomap_res;
+				new_frontier_marker.scale.x = octomap_res;
+				new_frontier_marker.scale.y = octomap_res;
+				new_frontier_marker.scale.z = octomap_res;
 				
-				marker.color.r = 1.0f;
-				marker.color.g = 0.0f;
-				marker.color.b = 0.0f;
-				marker.color.a = 1.0;
-				marker.lifetime = ros::Duration();
-				frontierMarkers.markers.push_back(marker);
+				new_frontier_marker.color.r = 1.0f;
+				new_frontier_marker.color.g = 0.0f;
+				new_frontier_marker.color.b = 0.0f;
+				new_frontier_marker.color.a = 1.0;
+				new_frontier_marker.lifetime = ros::Duration();
+				frontierMarkers.markers.push_back(new_frontier_marker);
 
         }
 		ROS_INFO("  publishing frontiers");
-        frontier_pub.publish(frontierMarkers);
+        frontier_publisher.publish(frontierMarkers);
 	}
 
 	std::vector<Frontier> sort_frontiers(std::vector<Point3D> frontiers_clustered, octomap::OcTree* octree){
@@ -280,17 +271,17 @@ public:
 		std::vector<Frontier> frontiers_sorted;
 		for(Point3D f1:frontiers_clustered){
 			Frontier frontier;
-			frontier.point = f1;
-			frontier.count = 0;
+			frontier.coordinates = f1;
+			frontier.neighborcount = 0;
 			for(Point3D f2:frontiers_clustered){
 				double d = euclideanDistance(f1,f2);
 				if(d < octomap_res){
-					frontier.count++;
+					frontier.neighborcount++;
 				}
 			}
 			bool addflag = true;
 			for(Frontier f3:frontiers_sorted){
-				if(euclideanDistance(f3.point,frontier.point) < octomap_res){
+				if(euclideanDistance(f3.coordinates,frontier.coordinates) < octomap_res){
 					addflag = false;
 				}
 			}
@@ -299,7 +290,7 @@ public:
 				for (int dy = -1; dy <= 1; ++dy) {
 				    for (int dz = -1; dz <= 1; ++dz) {
 				        if (dx != 0 && dy != 0 && dz != 0){
-				        	octomap::point3d point(frontier.point.x+octomap_res*dx, frontier.point.y+octomap_res*dy, frontier.point.z+octomap_res*dz);
+				        	octomap::point3d point(frontier.coordinates.x+octomap_res*dx, frontier.coordinates.y+octomap_res*dy, frontier.coordinates.z+octomap_res*dz);
 				        	octomap::OcTreeNode* result = octree->search(point);
 				        	if (result != nullptr) {
 						    	if (octree->isNodeOccupied(result)) {
@@ -312,8 +303,8 @@ public:
 			}
 			int entry_tol = 25;
 			bool add_entry_frontier = true;
-			//std::cout << drone_pos.x << "\t" << frontier.point.x << "\t" << cave_entry.x-entry_tol << "\t" << (drone_pos.x < cave_entry.x-entry_tol && frontier.point.x > cave_entry.x-entry_tol) << "\n";
-			if(drone_pos.x < cave_entry.x-entry_tol && frontier.point.x > cave_entry.x-entry_tol) {
+			//std::cout << curr_drone_position.x << "\t" << frontier.coordinates.x << "\t" << cave_entry_point.x-entry_tol << "\t" << (curr_drone_position.x < cave_entry_point.x-entry_tol && frontier.coordinates.x > cave_entry_point.x-entry_tol) << "\n";
+			if(curr_drone_position.x < cave_entry_point.x-entry_tol && frontier.coordinates.x > cave_entry_point.x-entry_tol) {
 				add_entry_frontier = false;
 			}
 			else{
@@ -321,15 +312,15 @@ public:
 			}
 			
 			
-			if(addflag && frontier.count > 120 && add_entry_frontier && nbscore < 3){
-				octomap::point3d drone_point(drone_pos.x, drone_pos.y, drone_pos.z);
-				octomap::point3d direction(frontier.point.x-drone_pos.x,frontier.point.y-drone_pos.y,frontier.point.z-drone_pos.z);
-				double d = euclideanDistance(drone_pos, frontier.point);
+			if(addflag && frontier.neighborcount > 120 && add_entry_frontier && nbscore < 3){
+				octomap::point3d drone_point(curr_drone_position.x, curr_drone_position.y, curr_drone_position.z);
+				octomap::point3d direction(frontier.coordinates.x-curr_drone_position.x,frontier.coordinates.y-curr_drone_position.y,frontier.coordinates.z-curr_drone_position.z);
+				double d = euclideanDistance(curr_drone_position, frontier.coordinates);
 				octomap::point3d end;
-				frontier.raytraceable = !octree->castRay(drone_point, direction, end, true, d);
-				//std::cout << frontier.raytraceable << "\n";
+				frontier.isReachable = !octree->castRay(drone_point, direction, end, true, d);
+				//std::cout << frontier.isReachable << "\n";
 				//std::cout << end << "\n";
-				//std::cout << frontier.point.x << "\t" << frontier.point.y << "\t" << frontier.point.z << "\n";
+				//std::cout << frontier.coordinates.x << "\t" << frontier.coordinates.y << "\t" << frontier.coordinates.z << "\n";
 				frontiers_sorted.push_back(frontier);
 			}
 		}
@@ -337,7 +328,7 @@ public:
 		return frontiers_sorted;
 	}
 	
-	
+//with partially help of Chatgpt, with the main focus to pase the binary octomap while Exploring cave
 // http://docs.ros.org/en/noetic/api/octomap_msgs/html/msg/Octomap.html
     void parseOctomap(const octomap_msgs::Octomap::ConstPtr& octomap_msg){ 
 		////Convert an octomap representation to a new octree (full probabilities or binary). You will need to free the memory. Return NULL on error. 
@@ -359,9 +350,9 @@ public:
 				                	octomap::OcTreeKey neighborKey(it.getKey().k[0] + dx, it.getKey().k[1] + dy, it.getKey().k[2] + dz);
 				                	if (octree->search(neighborKey)==NULL) {
 										Frontier frontierPoint;
-		                            	frontierPoint.point.x = it.getX();//+dx;
-		                            	frontierPoint.point.y = it.getY();//+dy;
-		                            	frontierPoint.point.z = it.getZ();//+dz;
+		                            	frontierPoint.coordinates.x = it.getX();//+dx;
+		                            	frontierPoint.coordinates.y = it.getY();//+dy;
+		                            	frontierPoint.coordinates.z = it.getZ();//+dz;
 		                            	frontiers.push_back(frontierPoint);
 		                       		}
 				                }
@@ -372,7 +363,7 @@ public:
             }
             
             //std::cout << frontiers.size() << "\n";
-            if(stm_state == "Explore Cave"){
+            if(statemachine_state == "Explore Cave"){
 		        std::vector<Point3D> frontierpoints_clustered;
 		        frontierpoints_clustered = meanShiftClustering(frontiers, 17.0, 1);
 		        
